95ce2e3637589f7b889e524aac48ca253759e257
Tommy <thomas.conf@ggl.com>
2022-11-21 10:57:16 +0000

    ammended mock test - not working

--- src/test/java/com/example/demoAPI/MockTest.java
+++ src/test/java/com/example/demoAPI/MockTest.java
@@ -1,76 +1,42 @@
 package com.example.demoAPI;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserController.class)
+
+// Fails to start, some default profile error
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@WebMvcTest
 public class MockTest {
 
-    //annotation, not decorator
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    public void createEmployeeAPI() throws Exception
-    {
-        mockMvc.perform( MockMvcRequestBuilders
-                        .post("/api/userName")
-                        .content(asJsonString(new UserModel("Johny", "Waaaa", 10, 20)))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.employeeId").exists());
-    }
+    @MockBean
+    private UserService userService;
 
-    public static String asJsonString(final Object obj) {
+    @Test
+    public void mockRootPath_ReturnsHelloWorld() throws Exception
+    {
         try {
-            return new ObjectMapper().writeValueAsString(obj);
+        mockMvc.perform(get("/api"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().string(containsString("Hello World")));
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 }
-
-/*
-@ExtendWith(MockitoExtension.class)
-//@RunWith(JUnitPlatform.class)
-public class MockTest
-{
-
-    @InjectMocks
-    UserController userController;
-
-    @Mock
-    UserService service;
-
-    @Test
-    public void testAddEmployee()
-    {
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-
-        when(employeeDAO.addEmployee(any(UserModel.class))).thenReturn(true);
-
-        UserModel user = new UserModel();
-        user.name = "Lokesh";
-        user.surname = "lkllk";
-        user.age = 11;
-        user.age2 = 22;
-
-
-        ResponseEntity<UserModel> responseEntity = userController.postBody(user);
-
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
-        //assertThat(responseEntity.getHeaders().getLocation().getPath()).isEqualTo("/1");
-    }
-
-}
-*/
 
